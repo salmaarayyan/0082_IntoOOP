3d2547a62e91bfbac13264faf0924e5627b38330
salmaarayyan <salmaarayyan@gmail.com>
2024-04-22 16:05:02 +0800

    menjalankan prosedur input dan output dari objek mk

--- OOP1.cpp
+++ OOP1.cpp
@@ -44,5 +44,10 @@ int main () {
     mhs.nama = "Budi";
     mhs.umur = 20;
     mhs.jurusan = "Teknik Informatika";
-    mhs.output();    
+    mhs.output();
+
+    mk.input();
+    mk.output();
+
+    return 0;    
 }
